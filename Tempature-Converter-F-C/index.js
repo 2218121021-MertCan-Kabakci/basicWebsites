736d076f13a317fdcd7de2ac7ef555434b23a7a6
Mert Can Kabakçı <125986530+2218121021-MertCan-Kabakci@users.noreply.github.com>
2024-07-18 15:18:17 +0300

    tempature converter
    fahrenhite to celcius
    celcius to fahrenhite

---
+++ Tempature-Converter-F-C/index.js
@@ -0,0 +1,22 @@
+
+const textBox = document.getElementById("textBox")
+const toFahrenheit = document.getElementById("toFahrenheit")
+const toCelcius = document.getElementById("toCelcius")
+const result = document.getElementById("result")
+let temp;
+
+function convert(){
+     if (toFahrenheit.checked) {
+          temp = Number(textBox.value);
+          temp = temp * 1.8 +32
+          result.textContent = temp + "°F"
+     }
+     else if(toCelcius.checked){
+          temp = Number(textBox.value);
+          temp = (temp-32)/1.8
+          result.textContent = temp + "°C"
+     }
+     else{
+          result.textContent = "select a unit"
+     }
+}
