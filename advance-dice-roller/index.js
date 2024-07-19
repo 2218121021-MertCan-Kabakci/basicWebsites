8cd9af5948bf24d043803405c10bd6754d65e042
Mert Can Kabakçı <125986530+2218121021-MertCan-Kabakci@users.noreply.github.com>
2024-07-19 19:03:52 +0300

    dice roller site with images

---
+++ advance-dice-roller/index.js
@@ -0,0 +1,17 @@
+
+function rollDice(){
+     const input = document.getElementById("input").value
+     const diceResult = document.getElementById("diceResult")
+     const diceImage = document.getElementById("diceImage")
+     const values = [];
+     const images = [];
+     for(let i=0; i<input;i++)
+     {        
+          const value = Math.floor(Math.random()*6) + 1
+          values.push(value);
+          images.push(`<img src="dice_images/d${value}.png" alt="Dice ${value}">`)
+     }
+     
+     diceResult.textContent = `dice: ${values.join(', ')}`
+     diceImage.innerHTML = `${images.join("")}`
+}
